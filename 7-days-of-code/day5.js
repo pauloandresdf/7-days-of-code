8546ea380c319047ab1bdffaf0ad615131afa992
Paulo <pauloandresdsf@gmail.com>
2025-03-03 21:43:50 -0300

    feat(add): resolvido desafio usando while e switch

---
+++ 7-days-of-code/day5.js
@@ -0,0 +1,47 @@
+let frutas = [];
+let laticionios = [];
+let congelados = [];
+let doces = [];
+let bebidas = [];
+let outros = [];
+let continuar = true;
+
+alert("Vamos fazer uma lista de compras!");
+
+while (continuar) {
+    let comida = prompt("Qual comida você deseja inserir?");
+    let categoria = prompt("Qual a categoria da comida que você deseja inserir? (frutas, laticionios, congelados, doces, bebidas, outros)");
+
+    switch(categoria) {
+        case "frutas":
+            frutas.push(comida);
+            break;
+        case "laticionios":
+            laticionios.push(comida);
+            break;
+        case "congelados":
+            congelados.push(comida);
+            break;
+        case "doces":
+            doces.push(comida);
+            break;
+        case "bebidas":
+            bebidas.push(comida);
+            break;
+        case "outros":
+            outros.push(comida);
+            break;
+        default:
+            console.log("Opção inválida!");
+            break;
+    }
+
+    continuar = prompt("Deseja adicionar mais alguma comida? (sim/não)") === "sim";
+}
+
+console.log("Frutas: ", frutas);
+console.log("Laticínios: ", laticionios);
+console.log("Congelados: ", congelados);
+console.log("Doces: ", doces);
+console.log("Bebidas: ", bebidas);
+console.log("Outros: ", outros);
